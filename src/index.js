9b7b33249cb3a40df343b7685aa3f5652fac9bef
skibyks <skiby_1@mail.ru>
2021-03-14 18:50:25 +0300

    fix: drackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+    '|':'|'
+  };
+  // в качестве стека в JavaScript можно использовать обычный массив
+  //(если пользоваться только «разрешенными» методами push & pop)
+  const st = [];
+  for (let i = 0; i < str.length; i++) {
+    if (isClosedBracket(str[i])) {
+      if (brackets[str[i]] !== st.pop()) return false;
+    // это открывающая скобка, т.к. других символов по условию в строке нет
+    } else {
+      st.push(str[i]);
+    }
+  }
+  return st.length === 0;
 }
+function isClosedBracket(ch) {
+  return [')', ']', '}'].indexOf(ch) > -1;
+}
+
